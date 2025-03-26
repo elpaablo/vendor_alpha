12ab1726592c30cae86ae14b920305a726857858
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 11:30:53 +0200

    soong: move extended compress format config to select
    Change-Id: Id56bad89607cd2961a571c148ec40e9334f0a6ce

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -69,7 +69,6 @@ SOONG_CONFIG_alphaNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += alphaQcomVars
 SOONG_CONFIG_alphaQcomVars += \
-    supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
@@ -87,7 +86,6 @@ SOONG_CONFIG_alphaGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLO
 SOONG_CONFIG_alphaGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_alphaGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_alphaNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
-SOONG_CONFIG_alphaQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_alphaQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_alphaGlobalVars_uses_oplus_touch := $(TARGET_USES_OPLUS_TOUCH)
 
