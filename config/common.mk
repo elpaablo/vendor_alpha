5cae252d237937a237fe48b3a95d0cd687cf68fc
elpaablo <donopwhile1@gmail.com>
2025-04-15 23:17:14 +0200

    Fix boot image profile location

--- config/common.mk
+++ config/common.mk
@@ -22,7 +22,7 @@ PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 WITH_DEXPREOPT_DEBUG_INFO := false
