24f5c3657ebc1f6bb07e71cc32dcc05daba0793d
Michael Bestas <mkbestas@lineageos.org>
2025-04-24 23:28:44 +0300

    config: Move bootloader message offset to select()
    Change-Id: Ic97af638005c299963d0aaa39e04227dcbca8751

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -30,7 +30,6 @@ SOONG_CONFIG_NAMESPACES += alphaGlobalVars
 SOONG_CONFIG_alphaGlobalVars += \
     aapt_version_code \
     additional_gralloc_10_usage_bits \
-    bootloader_message_offset \
     camera_needs_client_info_lib \
     camera_needs_client_info_lib_oplus \
     camera_override_format_from_reserved \
@@ -45,7 +44,6 @@ SOONG_CONFIG_alphaGlobalVars_camera_override_format_from_reserved := $(TARGET_CA
 SOONG_CONFIG_alphaGlobalVars_uses_oplus_touch := $(TARGET_USES_OPLUS_TOUCH)
 
 # Set default values
-BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED ?= false
 TARGET_CAMERA_SERVICE_EXT_LIB ?= libcameraservice_ext_lib
@@ -53,7 +51,6 @@ TARGET_CAMERA_SERVICE_EXT_LIB ?= libcameraservice_ext_lib
 # Soong value variables
 SOONG_CONFIG_alphaGlobalVars_aapt_version_code := $(shell date -u +%Y%m%d)
 SOONG_CONFIG_alphaGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
-SOONG_CONFIG_alphaGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_alphaGlobalVars_target_camera_package_name := $(TARGET_CAMERA_PACKAGE_NAME)
 SOONG_CONFIG_alphaGlobalVars_target_libcameraservice_ext_lib := $(TARGET_CAMERA_SERVICE_EXT_LIB)
 # Surfaceflinger
@@ -103,6 +100,11 @@ endif
 # Power HAL
 ifneq ($(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB),)
     $(call soong_config_set,power_libperfmgr,mode_extension_lib,$(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB))
+endif
+
+# Recovery
+ifneq ($(BOOTLOADER_MESSAGE_OFFSET),)
+    $(call soong_config_set,alpha_recovery,bootloader_message_offset,$(BOOTLOADER_MESSAGE_OFFSET))
 endif
 
 # Surfaceflinger
